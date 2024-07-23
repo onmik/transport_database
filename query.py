4dd223e99be0008439ba01626a2dd2a121e6cee0
onmik <58830586+onmik@users.noreply.github.com>
2024-07-23 16:03:16 +0200

    Add files via upload

--- query.py
+++ query.py
@@ -1,11 +1,14 @@
 import sqlite3
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 def get_task_by_id():
     try:
         with sqlite3.connect('transport.db') as conn:
             cur = conn.cursor()
-            cur.execute('SELECT "Kd mean" FROM Data WHERE Site = "Hrádek"')
+            cur.execute('SELECT "Kd mean" FROM Data WHERE Radionuclide_id = "Cs-137"')
+            #cur.execute('SELECT * FROM Water CROSS JOIN Sample')
             row = cur.fetchall()
             return row
     except sqlite3.Error as e:
@@ -16,4 +19,22 @@ if __name__ == '__main__':
     task = get_task_by_id()
 
 df = pd.DataFrame(task)
-print(df)
+dfnp = df.to_numpy().flatten()
+
+
+
+print(dfnp)
+print()
+print("min Kd", + np.min(dfnp))
+print("max Kd", + np.max(dfnp))
+print("mean Kd", + np.mean(dfnp))
+print("stdev Kd", + np.std(dfnp))
+
+plt.plot(dfnp)
+plt.show()
+
+hist, bin_edges = np.histogram(dfnp, density=True)
+
+plt.plot(hist)
+plt.hist(dfnp, bins=50)
+plt.show()
